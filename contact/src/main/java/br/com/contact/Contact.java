a21ee1e321fd19ffda729fec381f4021ae170197
Jobson Ribeiro de Souza <jobsonrisoa@gmail.com>
2022-03-14 09:55:58 -0300

    Added Controller

---
+++ contact/src/main/java/br/com/contact/Contact.java
@@ -0,0 +1,19 @@
+package br.com.contact;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+
+}
